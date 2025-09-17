70c7da9d60b70c5c157780145311118b8b7c4baf
jjuarez26base <jjuarez26@basecampcodingacademy.org>
2025-09-17 11:57:28 -0500

    Finished 2-Math and 3-Strings

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,2 +1,19 @@
 
-  
+let hourlyRate = 60;
+let dayRate = hourlyRate * 8;
+console.log(dayRate);
+
+let budget = 1000;
+let workdays = Math.floor(budget / dayRate);
+console.log(workdays);
+
+let billable = 22;
+let monthlyDiscount = 0.8;
+let fullMonths = Math.floor(35 / billable);
+console.log(fullMonths)
+let remainingDays = 35 - billable;
+let discountedMonthlyRate = billable * dayRate * monthlyDiscount;
+console.log(discountedMonthlyRate)
+let remainingCost = remainingDays * dayRate;
+let totalProjectCost = Math.round(discountedMonthlyRate + remainingCost);
+console.log(totalProjectCost)
---
+++ 3-Strings/jasmine/src/main.js
@@ -0,0 +1,17 @@
+
+let line = "Stands so high";
+let frontDoorLetter = line[0]
+console.log(frontDoorLetter);
+
+let frontDoorWord = "shire";
+let firstLetter = frontDoorWord.charAt(0);
+let remainingLetters = frontDoorWord.slice(1);
+let frontDoorPassword = firstLetter.toUpperCase() + remainingLetters;
+console.log(frontDoorPassword);
+
+let lineWithWhitespace = "Reins and harness"
+let backDoorLetter = lineWithWhitespace[16];
+console.log(backDoorLetter)
+
+let backDoorWord = 'Horse';
+let backDoorPassword = backDoorWord + ", please"
